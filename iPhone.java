b9ef0d414ccf1f72036bccfdc6d2d5ad30ae99e3
suelenschmidt <109631070+suelenschmidt@users.noreply.github.com>
2024-06-18 13:46:09 -0300

    Add files via upload

---
+++ iPhone.java
@@ -0,0 +1,69 @@
+public class iPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+
+    // Métodos da interface ReprodutorMusical
+    @Override
+    public void tocar() {
+        System.out.println("Reproduzindo música...");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Música pausada.");
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        System.out.println("Música selecionada: " + musica);
+    }
+
+    // Métodos da interface AparelhoTelefonico
+    @Override
+    public void ligar(String numero) {
+        System.out.println("Ligando para " + numero + "...");
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo chamada.");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz.");
+    }
+
+    // Métodos da interface NavegadorInternet
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo página: " + url);
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Nova aba adicionada.");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Página atualizada.");
+    }
+
+    public static void main(String[] args) {
+        iPhone meuIphone = new iPhone();
+
+        // Testando ReprodutorMusical
+        meuIphone.tocar();
+        meuIphone.pausar();
+        meuIphone.selecionarMusica("Imagine - John Lennon");
+
+        // Testando AparelhoTelefonico
+        meuIphone.ligar("123456789");
+        meuIphone.atender();
+        meuIphone.iniciarCorreioVoz();
+
+        // Testando NavegadorInternet
+        meuIphone.exibirPagina("https://www.example.com");
+        meuIphone.adicionarNovaAba();
+        meuIphone.atualizarPagina();
+    }
+}
